80036052e1b147bc62cb88bd20df685f81e6087d
jmcausing <johnmarkcausing@gmail.com>
2023-04-07 09:05:21 +0800

    updated app that accepts string/name

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/greet")
+def greet():
+    name = request.args.get("name")
+    if name:
+        return f"Hello, {name}!"
+    else:
+        return "Please provide a name."
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(port=8080)
