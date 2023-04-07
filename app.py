a71da8bb2224810bae8d55bbe6bd5596c86b580f
jmcausing <johnmarkcausing@gmail.com>
2023-04-07 09:09:01 +0800

    updated expose port

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -12,3 +12,7 @@ def greet():
 
 if __name__ == "__main__":
     app.run(port=8080)
+
+# When you run this script and navigate to http://localhost:8080/greet?name=John in your web browser, you should see the message "Hello, John!" displayed on the page. 
+#
+# If you navigate to http://localhost:8080/greet without providing a name parameter, you should see the message "Please provide a name.".    
