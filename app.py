8cdf65392dc2355888fc8b49f4bfe566acf81233
jmcausing <johnmarkcausing@gmail.com>
2023-04-07 09:11:32 +0800

    updated expose port

--- app.py
+++ app.py
@@ -2,17 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name")
-    if name:
-        return f"Hello, {name}!"
-    else:
-        return "Please provide a name."
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(port=8080)
-
-# When you run this script and navigate to http://localhost:8080/greet?name=John in your web browser, you should see the message "Hello, John!" displayed on the page. 
-#
-# If you navigate to http://localhost:8080/greet without providing a name parameter, you should see the message "Please provide a name.".    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
